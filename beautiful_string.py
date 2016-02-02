6f113d5bf146851b9e3422ada5e770eaa3a44236
The Diabolical Lingqiang Kong <konglq@gmail.com>
2016-02-02 07:24:54 +0000

    update beautiful string with correct answer

--- beautiful_string.py
+++ beautiful_string.py
@@ -3,21 +3,22 @@
 https://www.codeeval.com/open_challenges/83/
 '''
 
-import string
 from collections import defaultdict
 
 lines = ['ABbCcc',
 'Good luck in the Facebook Hacker Cup this year!',
 'Ignore punctuation, please :)',
 'Sometimes test cases are hard to make up.',
-'So I just go consult Professor Dalves']
+'So I just go consult Professor Dalves',
+"""+_W|F!WwA+n3i|~jV?2%|<YhBxg$5?OLtOCeqfEW9!ICcD,a;bU/69T9V:-5l`\EU$;@]]>xQE\i>S1ehq@btF`I0v@,;5p6WB`3$;pNG!=D#+)eKPr\['UVs,+mHb:7}UOnL'(;`,'f(_[ZIh_Jh|"A8[XB;U]yyv4"Cwhc[zSV0!JVXOEYShNlSt/8Y|Q1i)G~m.izy>3~['yX7yliWx1%rAJ;`Oc33oC?@B)lL95&"""]
 
 for line in lines:
   if line:
-    line = line.translate(None,string.punctuation)
+    # leave only the alpha characters (no numerals)
+    line = ''.join([c for c in line if c.isalpha()])
 
     beauty_dict = defaultdict(int)
-    for letter in line.replace(' ','').lower():
+    for letter in line.lower():
       beauty_dict[letter] += 1
 
   beauty = 0
