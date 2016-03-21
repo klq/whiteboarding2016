6b31073ddca43c23be19579b4f7309ef363effe6
Lingqiang Kong <konglq@gmail.com>
2016-03-21 11:11:08 -0700

    add bridge problem (unsolved)

---
+++ bridge_problem.py
@@ -0,0 +1,27 @@
+'''
+Bridge crossing
+
+N walkers need to cross a footbridge. Each person has a specific time it would take them to cross the bridge. Atmost two people can go together (and in that case, the time taken will be the maximum of the two). Also, they need a flashlight to cross and there is only one flashlight available.
+
+Given a list of times for the N walkers, compute the minimum time needed for the entire group to cross.
+
+For example, given [1,2,3]. 1 & 2 would cross (time taken = 2). 1 will come back with the flashlight (time taken = 1). 1 & 3 would cross (time taken =3). So a total of 2+1+3=6.
+
+This is a challenging problem. Your solution doesn't have to get the minimum time. Come up with some heuristic and compute the crossing time for it. Then see if you can improve your heuristic.
+'''
+
+#if s2 >= 2F2-f1, thenorigin
+
+def bridge_problem():
+  pass
+
+
+total_time = 0
+
+times = [1,1,2,4,88,90,100]
+origin_times = times
+destination_times = []
+
+
+
+
